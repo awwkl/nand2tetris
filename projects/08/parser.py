3ea1481438b8735a3450eb6ae71b59c85fdbeae0
sky-aw <sky99aw@gmail.com>
2020-05-02 21:40:01 +0800

    done program flow portion of 08

---
+++ projects/08/parser.py
@@ -0,0 +1,9 @@
+def parse(line):
+    line = line.strip()
+
+    if line.startswith("//") or line == "":     # comment line or empty line
+        return None
+    
+    line = line.split("/")[0].strip()       # removes trailing comments e.g. in "pop local 0    // initializes sum = 0"
+
+    return line.split(" ")                  # returns array - ["push", "local", "3"]
